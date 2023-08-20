5cac1dd7e46a45cc2b8d47fdd1d72c67e22cd293
akmaalll <akmalrayadilfitrah@gmail.com>
2023-08-20 22:28:45 +0800

    tes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
-            $table->string('username');
+            $table->string('name');
+            $table->string('email');
             $table->string('password');
             $table->timestamps();
         });
