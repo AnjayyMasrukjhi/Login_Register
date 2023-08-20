ecc6367f825486a04732a32216ab3c8bca8a211f
akmaalll <akmalrayadilfitrah@gmail.com>
2023-08-20 21:55:41 +0800

    add controller login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+use DB;
+use App\Models\User;
+use Carbon\Carbon;
+use Session;
+use Brian2694\Toastr\Facades\Toastr;
+
+class LoginController extends Controller
+{
+    
+}
